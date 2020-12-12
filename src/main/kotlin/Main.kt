39c86b1ef542ee9ab0537c1df736da2e5827cfdb
Nuno Pereira21702354 <a21702354@alunos.ulht.pt>
2020-12-12 20:15:54 +0000

    Erro corrigido

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+import java.lang.NumberFormatException
+import java.util.*
+import kotlin.system.exitProcess
+
+
+fun main() {
+
+    var isRunning = true
+
+    while (isRunning) {
+
+            buildMenu()
+
+        try {
+
+            val scan = Scanner(System.`in`)
+            val choice = scan.next()
+
+            if (checkIsNumber(choice)) {
+
+                when (choice.toInt()) {
+
+                    1 -> {
+
+                        isRunning = false
+                        startGame()
+
+
+                    }
+
+                    0 -> exitProcess(1)
+                    else ->
+                        print("\n" + "Option Not Available" + "\n")
+
+                }
+
+
+            }
+
+        } catch (e: NumberFormatException) {
+
+            print("Error. You have to choose a number!")
+
+        }
+
+    }
+}
+
+
