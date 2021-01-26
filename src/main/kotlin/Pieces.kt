e00eb8b19862d5744e56b3c86d63f6aa18048544
marcelogdomingues <marcelo.domigues.ul@gmail.com>
2021-01-26 17:14:02 +0000

    Added pieces

---
+++ src/main/kotlin/Pieces.kt
@@ -0,0 +1,11 @@
+fun wantSpecificPiece(message: String): Boolean? {
+    return false
+}
+
+fun isASpecificPieceValid(piece: String): Boolean{
+    return false
+}
+
+fun replaceWithSpecificPiece(pieces : Array<Pair<String,String>?>, piece: String){
+
+}
